6d828d0f54b7fc69b200dc235c1108a8507853c9
Emrah SARI <emrahsari@msn.com>
2015-06-06 04:47:42 +0300

    no message

---
+++ js/bootcards.js
@@ -0,0 +1,105 @@
+var bootcards = bootcards || {};
+
+bootcards._isXS = null;
+
+/*
+ * Get the Bootstrap enviroment we're in.
+ * Found at http://stackoverflow.com/questions/14441456/how-to-detect-which-device-view-youre-on-using-twitter-bootstrap-api/15150381#15150381
+ * Function writen by Raphael_ (http://stackoverflow.com/users/1661358/raphael)
+ */
+bootcards.findBootstrapEnvironment = function() {
+    var envs = ["ExtraSmall", "Small", "Medium", "Large"];
+    var envValues = ["xs", "sm", "md", "lg"];
+
+    $el = $('<div>');
+    $el.appendTo($('body'));
+
+    for (var i = envValues.length - 1; i >= 0; i--) {
+        var envVal = envValues[i];
+
+        $el.addClass('hidden-'+envVal);
+        if ($el.is(':hidden')) {
+            $el.remove();
+            return envs[i];
+        }
+    }
+};
+
+bootcards.isXS = function() {
+    if (this._isXS === null ) {
+        this._isXS = (this.findBootstrapEnvironment() == "ExtraSmall");
+    }
+    return this._isXS;
+};
+
+/*
+ * Disable rubberbanding effect in iOS.
+ * Based on the 'Baking Soda Paste' technique written by Armagan Amcalar at
+ * http://blog.armaganamcalar.com/post/70847348271/baking-soda-paste
+ */
+bootcards.disableRubberBanding = function() {
+   document.body.addEventListener('touchstart', function() {
+        document.body.addEventListener('touchmove', function moveListener(e) {
+            document.body.removeEventListener('touchmove', moveListener);
+
+            var el = e.target;
+
+            do {
+
+                var h = parseInt(window.getComputedStyle(el, null).height, 10);
+                var sH = el.scrollHeight;
+
+                if (h < sH) {
+                    return;
+                }
+            } while (el != document.body && el.parentElement != document.body && (el = el.parentElement));
+
+            e.preventDefault();
+        });
+    });
+
+};
+
+/*
+ * Initialize an off-canvas menu. This takes 4 arguments:
+ * - the element used to toggle the off canvas menu
+ * - the off canvas element to slide in
+ * - the main content area to push away when the off canvas element slides in
+ * - a boolean indicating if the off canvas menu should be hidden when the main content area is clicked
+ * 
+ * An off canvas menu is required for the portrait-single-pane mode
+ *
+ */
+bootcards.initOffCanvasMenu = function(offCanvasToggleEl, offCanvasMenuEl, mainContentEl, hideOnMainClick) {
+
+    this.offCanvasToggleEl = offCanvasToggleEl;
+    this.offCanvasMenuEl = offCanvasMenuEl;
+    this.mainContentEl = mainContentEl;
+
+    offCanvasToggleEl.on("click", function() {
+      bootcards.offCanvasMenuEl.toggleClass("active");
+      if (bootcards.mainContentEl) { bootcards.mainContentEl.toggleClass("active"); }
+    });
+
+    //hide the offcanvas if you click on the body
+    if (hideOnMainClick && this.mainContentEl) {
+        this.mainContentEl.on("click", function() {
+            var $this = $(this);
+            if ($this.hasClass('active') ) {
+                $this.removeClass('active');
+                bootcards.offCanvasMenuEl.removeClass('active');
+            }
+
+        });
+    }
+
+};
+
+//hide the offcanvas menu
+bootcards.hideOffCanvasMenu = function() {
+
+    this.offCanvasMenuEl.removeClass('active');
+    this.mainContentEl.removeClass('active');
+
+};
+
---
+++ js/main.ios.js
@@ -0,0 +1,108 @@
+$('.cropme').simpleCropper();
+$(document).ready(function() {
+
+    ion.sound({
+            sounds: [
+                {name: "glass"},
+                {name: "tap"}
+            ],
+            path: "http://cdn.royalbits.com/sounds/",
+            preload: true,
+            volume: 1.0
+        });
+
+    $('.multiselect').multiselect({
+        enableFiltering: true,
+        numberDisplayed: 0,
+        nSelectedText: 'adrese kart gönderilecek.',
+        nonSelectedText: 'Defterden seçim yapılmadı. Adres ekleyiniz!',
+        buttonWidth: '100%',
+        filterPlaceholder: 'Ara',
+        onChange: function(option, checked) {
+	        var selected = 0;
+	        $('option', $('#addresses')).each(function() {
+	            if ($(this).prop('selected')) {
+	                selected++;
+	            }
+	        });
+	        if (selected == 1) {
+	        	$('#new_address').hide();
+	        }
+	        if (selected == 0) {
+	        	$('#new_address').show();
+	        }
+   		}
+    });
+
+    $('textarea[id^="textarea"]').maxlength({
+		alwaysShow: true,
+     	warningClass: 'label label-warning',
+     	limitReachedClass: 'label label-success'
+    });
+
+});
+
+(function($){ 
+    $(window).load(function(){
+        $("[id^='list-group_']").mCustomScrollbar({setHeight:329,scrollbarPosition:"outside",autoHideScrollbar:true});
+    });
+ })(jQuery);
+
+// $("#headerCarousel").backstretch("http://cdn.royalbits.com/img/test/img_banner_header.png");
+// $("#footerCarousel").backstretch("http://cdn.royalbits.com/img/test/img_banner_goggles.png");
+
+$("#headerCarousel").backstretch("https://farm4.staticflickr.com/3859/15174866990_6d4129cb35_o.png");
+$("#footerCarousel").backstretch("https://farm3.staticflickr.com/2944/15361248622_60c345dc09_o.png");
+
+// $("#headerCarousel").backstretch("https://farm4.staticflickr.com/3859/15174866990_13c724415b_b.jpg");
+// $("#footerCarousel").backstretch("https://farm3.staticflickr.com/2944/15361248622_6373a30509_b.jpg");
+
+$("[id^='collapse-']").on('shown.bs.collapse', function () {
+    jQuery("[id^='list-group_']").mCustomScrollbar("scrollTo","bottom")
+})
+
+function init() { initWebSocket(); }  
+function initWebSocket() { 
+    websocket = new WebSocket(wsUri); 
+    websocket.onopen = function(evt) { onOpen(evt) }; 
+    websocket.onclose = function(evt) { onClose(evt) }; 
+    websocket.onmessage = function(evt) { onMessage(evt) }; 
+    websocket.onerror = function(evt) { onError(evt) }; 
+}
+function onOpen(evt) { 
+    console.log("CONNECTED");
+}  
+function onClose(evt) { 
+    console.log("DISCONNECTED"); 
+}  
+function onMessage(evt) {
+    evt_counter = evt_counter + 1;
+    var obj = jQuery.parseJSON(evt.data);
+    if(obj.w == '1'){
+        ion.sound.play("glass");
+        if(document.getElementById("panel-heading_"+obj.i)){
+            jQuery.ajax({type:'GET', url:'/support/message/'+obj.i,success:function(data,textStatus){jQuery('#message_'+obj.i).html(data);},error:function(XMLHttpRequest,textStatus,errorThrown){},complete:function(XMLHttpRequest,textStatus){  jQuery("#list-group_"+obj.i).mCustomScrollbar("scrollTo","bottom") }});
+            document.getElementById("panel-heading_"+obj.i).style.background = "#A6FFB1";
+        }
+    }
+    if(obj.w == '0'){
+        if(document.getElementById("panel-heading_"+obj.i)){
+            jQuery.ajax({type:'GET', url:'/support/message/'+obj.i,success:function(data,textStatus){jQuery('#message_'+obj.i).html(data);},error:function(XMLHttpRequest,textStatus,errorThrown){},complete:function(XMLHttpRequest,textStatus){  jQuery("#list-group_"+obj.i).mCustomScrollbar("scrollTo","bottom") }});
+        }
+    }
+    if(obj.o == '0'){
+        document.getElementById("panel-heading_"+obj.i).style.background = "#fefefe";
+        document.getElementById("content_"+obj.i).style.display = 'none';
+        document.getElementById("button_"+obj.i).style.display = 'none';
+        document.getElementById("loading_"+obj.i).innerHTML = 'Destek konusu kapatıldı.';
+    }
+} 
+function onError(evt) { 
+    console.log(evt.data); 
+}  
+window.addEventListener("load", init, false);
+
+$(window).on('resize', function(){
+    $("#upload").css("height", $("#upload").width()/1.48);
+    $(".upload-photo").css("height", $(".upload-photo").width()/1.48);
+}).trigger('resize');
